2e98082a4b8f69a438f999126b4409f9f939017d
Mark Seger <mark.seger@hp.com>
2016-05-24 07:39:11 -0400

    updated to V0.2.1

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup
 
 setup(name='gptools',
-      version='0.1.3',
+      version='0.2.1',
       description='getput tools',
       author='Mark Seger',
-      author_email='mark.seger@hp.com',
+      author_email='mark.seger@hpe.com',
       url='https://github.com/markseger/getput',
 
       data_files=[('/usr/bin',['getput','gpmulti','gpsuite']),
                   ('/etc/gpsuite.d', ['gpsuite.conf']),
-                  ('/usr/share/doc/gptools',['getting-started.txt','RELEASE-gptools','Introduction.pdf']),
+                  ('/usr/share/doc/gptools',['Getting-Started.txt','RELEASE-gptools','Introduction.pdf']),
                   ('/usr/share/man/man1',['getput.1', 'gpmulti.1', 'gpsuite.1'])]
      )
