a30dc1d1e6ded1ca5b04b44f6c17ef22d4693e36
Mark Seger <Mark.Seger@hpe.com>
2017-08-15 08:18:03 -0400

    --bumped version

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup
 
 setup(name='gptools',
-      version='0.2.4',
+      version='0.2.6',
       description='getput tools',
       author='Mark Seger',
-      author_email='mark.seger@hpe.com',
+      author_email='mseger@suse.com',
       url='https://github.com/markseger/getput',
 
       data_files=[('/usr/bin',['getput','gpmulti','gpsuite','gpsum','gpwhere']),
                   ('/etc/gpsuite.d', ['gpsuite.conf']),
                   ('/usr/share/doc/gptools',['getting-started.txt','RELEASE-gptools','Introduction.pdf']),
                   ('/usr/share/man/man1',['getput.1', 'gpmulti.1', 'gpsuite.1', 'gpsum.1', 'gpwhere.1'])]
-     )
+     
